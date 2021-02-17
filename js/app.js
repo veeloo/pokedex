98f94ce05e153fd2c1e55bfbc6583624bb55f806
christianngouw <christiangouw168@gmail.com>
2021-02-17 21:26:05 +0700

    code refactoring, fixing styling, & adding Vue.js

--- js/app.js
+++ js/app.js
@@ -22,15 +22,11 @@ function getPokemon(id) {
     fetch(`${baseURL}/${id}`)
         .then((res) => res.json())
         .then((data, index) => {
-
             
             pokeMoves = data.moves;
 
             let name = data.name.charAt(0).toUpperCase() + data.name.slice(1);
             let type = data.types[0].type.name.charAt(0).toUpperCase() + data.types[0].type.name.slice(1)
-
-
-
 
             el.innerHTML = `
                 <div class='text-center'>
@@ -46,7 +42,6 @@ function getPokemon(id) {
                     </div>
 
                     <div class="p-5 ">                        
-
                         <div class="my-2">
                             <div class="text-5xl mb-2 font-bold text-gray-700 mt-2">${name}</div>                                                                                
                             <span class='text-green-400 font-extrabold text-lg font-mono'>${type}</span>                        
@@ -57,18 +52,12 @@ function getPokemon(id) {
                                 <div class='hover:text-gray-500 inline-block'>${data.abilities[0].ability.name}</div>                        
                                 <div class='hover:text-gray-500 inline-block'>${data.abilities[1].ability.name}</div>                        
                             </div>                            
-                        </div>
-
-                        <div class='mt-5 text-right'>                            
-                            <div class=' text-blue-500 hover:text-blue-700 duration-200 select-none cursor-pointer' onclick="showPokemonDetail()" >Show Detail</div>
-                        </div>
-                    </div>   
+                        </div>                        
+                    </div>
                 </div>
 
-                <div class='text-left hidden ml-10' id='pokemon-detail-container'>
-
-                    <div class='cursor-pointer mb-5 text-right text-red-500 hover:text-red-600 select-none ' onclick="closePokemonDetail()">Close</div>
-
+                <div class='text-left  ml-10' id='pokemon-detail-container'>
+                    
                     <div class="mb-5 font-mono">
                         <div class='mb-1 font-sans font-bold text-gray-700'>General</div>
                         <div class='inline-block mr-5'>Height: <span>${data.height}m</span> </div>
@@ -83,18 +72,11 @@ function getPokemon(id) {
 
                     <div class="mb-5 font-mono flex-wrap flex">
                         
-                        
                     </div>
-                    
-                
-                    
-
                 </div>
 
             `;
         });
-
-
 }
 
 const showPokemonDetail = () => {    
---
+++ js/index.js
@@ -0,0 +1,26 @@
+
+const app = new Vue({       
+    el : "#app",        
+    data : {
+        baseURL : 'https://pokeapi.co/api/v2/pokemon/',                
+        pokemonData : '',    
+        pokemonId : '',
+    },
+    mounted(){
+        this.getPokemon()
+    },
+    methods : {
+        randomPokeId(){            
+            return Math.floor(Math.random() * 150) +1;            
+        },
+        getPokemon(){
+            fetch(`${this.baseURL}`+this.randomPokeId())
+            .then(res => res.json())
+            .then(data => {
+                this.pokemonData = data     
+                let pokemonId = data.id+""            
+                this.pokemonId = pokemonId.padStart(3,"0")
+            })
+        }
+    }
+})
