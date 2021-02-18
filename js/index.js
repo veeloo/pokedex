93647e6d42db17bbdbf9d53abe587aa60fb9eb8d
christianngouw <christiangouw168@gmail.com>
2021-02-19 00:43:32 +0700

    adding new endpoint request

--- js/index.js
+++ js/index.js
@@ -2,10 +2,11 @@
 const app = new Vue({       
     el : "#app",        
     data : {
-        baseURL : 'https://pokeapi.co/api/v2/pokemon/',
-        pokemonData : '',    
-        pokemonId : '',
-
+        baseURL : 'https://pokeapi.co/api/v2/',
+        pokemon : '',    
+        pokemonSpecies : '',
+        pokemonId : '',          
+        pokemonEvolutionChain : [],
     },
     mounted(){
         this.getPokemon()
@@ -15,13 +16,28 @@ const app = new Vue({
             return Math.floor(Math.random() * 150) +1;            
         },
         getPokemon(){
-            fetch(`${this.baseURL}`+1)
+
+            // 
+            fetch(`${this.baseURL}pokemon/`+1)
             .then(res => res.json())
             .then(data => {
-                this.pokemonData = data     
+                this.pokemon = data     
                 let pokemonId = data.id+""            
                 this.pokemonId = pokemonId.padStart(3,"0")
             })
+        
+            fetch(`${this.baseURL}pokemon-species/`+1)
+            .then(res => res.json())
+            .then(data => {                
+                this.pokemonSpecies = data
+            })
+
+            fetch(`${this.baseURL}evolution-chain/`+1)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+
         }
     }
 })
